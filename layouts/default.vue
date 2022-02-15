a902e80a5eb227589bc7d7e8634035320a6b360f
JiangFFFF <1820920433@qq.com>
2022-02-15 22:03:59 +0800

    2.15-第12天

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,11 +5,11 @@
     <header id="header">
       <section class="container">
         <h1 id="logo">
-          <a href="#" title="阿昌之谷粒学院">
+          <a href="#" title="谷粒学院">
             <img
               src="~/assets/img/logo.png"
               width="100%"
-              alt="阿昌之谷粒学院"
+              alt="谷粒学院"
             />
           </a>
         </h1>
@@ -32,44 +32,45 @@
             </router-link>
           </ul>
           <!-- / nav -->
+          <!-- / nav -->
           <ul class="h-r-login">
-            <li id="no-login">
-              <a href="/sing_in" title="登录">
-                <em class="icon18 login-icon">&nbsp;</em>
-                <span class="vam ml5">登录</span>
-              </a>
-              |
-              <a href="/sign_up" title="注册">
-                <span class="vam ml5">注册</span>
-              </a>
-            </li>
-            <li class="mr10 undis" id="is-login-one">
-              <a href="#" title="消息" id="headerMsgCountId">
-                <em class="icon18 news-icon">&nbsp;</em>
-              </a>
-              <q class="red-point" style="display: none">&nbsp;</q>
-            </li>
-            <li class="h-r-user undis" id="is-login-two">
-              <a href="#" title>
-                <img
-                  src="~/assets/img/avatar-boy.gif"
-                  width="30"
-                  height="30"
-                  class="vam picImg"
-                  alt
-                />
-                <span class="vam disIb" id="userName"></span>
-              </a>
-              <a
-                href="javascript:void(0)"
-                title="退出"
-                onclick="exit();"
-                class="ml5"
-                >退出</a
-              >
-            </li>
-            <!-- /未登录显示第1 li；登录后显示第2，3 li -->
+              <li v-if="!loginInfo.id" id="no-login">
+                  <a href="/login" title="登录">
+                      <em class="icon18 login-icon">&nbsp;</em>
+                      <span class="vam ml5">登录</span>
+                  </a>
+                  <a href="/register" title="注册">
+                      <span class="vam ml5">注册</span>
+                  </a>
+              </li>
+              <li v-if="loginInfo.id" id="is-login-one" class="mr10">
+                  <a id="headerMsgCountId" href="#" title="消息">
+                      <em class="icon18 news-icon">&nbsp;</em>
+                  </a>
+                  <q class="red-point" style="display: none">&nbsp;</q>
+              </li>
+              <li v-if="loginInfo.id" id="is-login-two" class="h-r-user">
+                  <a href="/ucenter" title>
+                      <img
+                          :src="loginInfo.avatar"
+                          width="30"
+                          height="30"
+                          class="vam picImg"
+                          alt
+                          />
+                      <span id="userName" class="vam disIb">
+                        {{loginInfo.nickname}}</span>
+                  </a>
+                  <a
+                    href="javascript:void(0);"
+                    title="退出"
+                    @click="logout()"
+                    class="ml5"
+                    >退 出</a>
+              </li>
+              <!-- /未登录显示第1 li；登录后显示第2，3 li -->
           </ul>
+
           <aside class="h-r-search">
             <form action="#" method="post">
               <label class="h-r-s-box">
@@ -105,8 +106,8 @@
           </h4>
           <ul class="of flink-list">
             <li>
-              <a href="http://www.atguigu.com/" title="阿昌" target="_blank"
-                >阿昌</a
+              <a href="http://www.atguigu.com/" title="姜惠峰" target="_blank"
+                >姜惠峰</a
               >
             </li>
           </ul>
@@ -124,7 +125,7 @@
                 <span>Email：info@atguigu.com</span>
               </section>
               <section class="b-f-link mt10">
-                <span>©2021阿昌之谷粒学院</span>
+                <span>©2021谷粒学院</span>
               </section>
             </section>
           </section>
@@ -152,5 +153,55 @@ import "~/assets/css/reset.css";
 import "~/assets/css/theme.css";
 import "~/assets/css/global.css";
 import "~/assets/css/web.css";
-export default {};
+import cookie from 'js-cookie'
+
+export default {
+  data(){
+    return{
+      token:'',
+      loginInfo:{
+        id:'',
+        age:'',
+        avatar:'',
+        mobile:'',
+        nickname:'',
+        sex:''
+      }
+    }
+  },
+  methods:{
+    //账号退出
+    logout(){
+      this.$confirm('此操作将退出当前账号, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => { //点击确定，执行then方法
+            //清空cookie中的值
+            cookie.set('guli_token','',{domain:'localhost'})
+            cookie.set('guli_ucenter','',{domain:'localhost'})
+            //跳转首页面
+            window.location.href="/"
+        }).catch(() => { //点击取消，执行catch方法
+          this.$message({
+            type: 'info',
+            message: '已取消退出'
+          });          
+        })
+    },
+
+    //创建方法，从cookie中获取用户信息
+    showInfo(){
+      var userStr=cookie.get('guli_ucenter')
+      //把后端传来的字符串转换为前端的json对象
+      if(userStr){
+        this.loginInfo=JSON.parse(userStr)
+      }
+      
+    }
+  },
+  created(){
+    this.showInfo()
+  }
+};
 </script>
--- utils/request.js
+++ utils/request.js
@@ -1,7 +1,28 @@
 //封装axios
 import axios from 'axios'
+import cookie from 'js-cookie'
+import { MessageBox,Message } from 'element-ui'
 // 创建axios实例
 const service = axios.create({
 baseURL: 'http://localhost:9001', // api的base_url timeout: 20000 // 请求超时时间
 })
+
+//第三步：创建拦截器
+//每次请求中使用拦截器
+service.interceptors.request.use(
+    config => {
+        //debugger
+        //判断cookie中是否有名称叫 guli_token的数据
+        if (cookie.get('guli_token')) {
+            //把获取到的cookie值放到header中,主要放在header中是为了调接口使用
+            config.headers['token'] = cookie.get('guli_token');
+        }
+        return config
+    },
+    err => {
+        return Promise.reject(err);
+    }
+)
+
 export default service
+
